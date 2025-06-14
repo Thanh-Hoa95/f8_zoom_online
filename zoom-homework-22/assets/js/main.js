a18e056c19d0f132044d5b127e1fc1f412929f68
Nguyễn Thanh Hòa <nguyenthanhhoa075@gmail.com>
2025-06-14 22:56:07 +0700

    Nội dung cập nhật

--- zoom-homework-22/assets/js/main.js
+++ zoom-homework-22/assets/js/main.js
@@ -2,60 +2,167 @@ const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
 const addBtn = $(".add-btn");
-const formAdd = $("#addTaskModal");
+const formApp = $("#addTaskModal");
 const modalClose = $(".modal-close");
 const btnClose = $(".btn-close");
-const todoFrom = $(".todo-app-form");
-const formInput = $("#taskTitle");
+const todoForm = $(".todo-app-form");
+const taskTitle = $("#taskTitle");
+const todoList = $("#todoList");
+const searchInput = $(".search-input");
 
-let todoTask = [];
-
-function closeForm() {
-  formAdd.className = "modal-overlay";
-}
-
-function openForm() {
-  formAdd.className = "modal-overlay show";
-  setTimeout(() => formInput.focus(), 300);
-}
-openForm();
-
-addBtn.onclick = openForm;
-modalClose.onclick = closeForm;
-btnClose.onclick = closeForm;
-
-todoFrom.onsubmit = function (event) {
-  event.preventDefault();
-
-  const newTask = Object.fromEntries(new FormData(todoFrom));
-  newTask.isCompleted = false;
-
-  todoFrom.reset();
-  closeForm();
-
-  todoTask.push(newTask);
-  renderTask(todoTask);
+searchInput.oninput = function (event) {
+  console.log(event.target.value);
 };
 
-function renderTask(tasks) {
-  const html = tasks
+let editIndex = null;
+/*
+git clone git@github-HaTrang:Thanh-Hoa95/f8_zoom_online.git
+
+*/
+addBtn.onclick = function (event) {
+  handleOpenModal();
+
+  setTimeout(() => taskTitle.focus(), 100);
+};
+
+todoList.onclick = function (event) {
+  const editBtn = event.target.closest(".edit-btn");
+  const deleteBtn = event.target.closest(".delete-btn");
+  const completeBtn = event.target.closest(".complete-btn");
+
+  if (editBtn) {
+    const taskIndex = editBtn.dataset.index;
+    const task = todoTask[taskIndex];
+
+    editIndex = taskIndex;
+
+    for (const key in task) {
+      const value = task[key];
+      const input = $(`[name="${key}"]`);
+
+      if (input) {
+        input.value = value;
+      }
+    }
+    handleOpenModal();
+    const formTitle = formApp.querySelector(".modal-title");
+
+    if (formTitle) {
+      formTitle.dataset.original = formTitle.textContent;
+
+      formTitle.textContent = "Edit Task";
+    }
+
+    const btnSubmit = formApp.querySelector(".btn-submit");
+
+    if (btnSubmit) {
+      btnSubmit.dataset.original = btnSubmit.textContent;
+      btnSubmit.textContent = "Save";
+    }
+  }
+
+  if (deleteBtn) {
+    const taskIndex = deleteBtn.dataset.index;
+    const task = todoTask[taskIndex];
+    if (confirm(`Bạn có muốn xóa công việc ${task.title} không?`)) {
+      todoTask.splice(taskIndex, 1);
+      saveTodoTask();
+      renderTask();
+    }
+  }
+
+  if (completeBtn) {
+    const taskIndex = completeBtn.dataset.index;
+    const task = todoTask[taskIndex];
+    task.isCompleted = !task.isCompleted;
+    saveTodoTask();
+    renderTask();
+  }
+};
+
+function handleOpenModal() {
+  formApp.classList.add("show");
+}
+
+function handleCloseModal() {
+  formApp.classList.remove("show");
+
+  if (editIndex) {
+    const formTitle = formApp.querySelector(".modal-title");
+
+    if (formTitle) {
+      formTitle.textContent = formTitle.dataset.original;
+      delete formTitle.dataset.original;
+    }
+
+    const btnSubmit = formApp.querySelector(".btn-submit");
+
+    if (btnSubmit) {
+      btnSubmit.textContent = btnSubmit.dataset.original;
+      delete btnSubmit.dataset.original;
+    }
+  }
+
+  formApp.querySelector(".modal").scrollTop = 0;
+
+  todoForm.reset();
+  editIndex = null;
+}
+
+modalClose.onclick = function (event) {
+  handleCloseModal();
+};
+
+btnClose.onclick = function (event) {
+  handleCloseModal();
+};
+
+const todoTask = JSON.parse(localStorage.getItem("todoTasks")) ?? [];
+
+todoForm.onsubmit = function (event) {
+  event.preventDefault();
+
+  const formData = Object.fromEntries(new FormData(todoForm));
+
+  if (editIndex) {
+    todoTask[editIndex] = formData;
+  } else {
+    formData.isCompleted = false;
+    todoTask.push(formData);
+  }
+  handleCloseModal();
+  saveTodoTask();
+  renderTask();
+};
+
+function saveTodoTask() {
+  localStorage.setItem("todoTasks", JSON.stringify(todoTask));
+}
+
+function renderTask() {
+  if (!todoTask.length) {
+    todoList.innerHTML = `<h3 class="no-task">Chưa có công việc!!!</h3>`;
+    return;
+  }
+
+  const html = todoTask
     .map(
-      (task) => `
-  <div class="task-card ${task.color} ${task.isCompleted ? "completed" : ""}">
+      (task, index) => `
+<div class="task-card ${task.isCompleted ? "completed" : ""}" style="--card-color:${task.color}" >
           <div class="task-header">
             <h3 class="task-title">${task.title}</h3>
             <button class="task-menu">
               <i class="fa-solid fa-ellipsis fa-icon"></i>
               <div class="dropdown-menu">
-                <div class="dropdown-item">
+                <div class="dropdown-item edit-btn" data-index= "${index}">
                   <i class="fa-solid fa-pen-to-square fa-icon"></i>
                   Edit
                 </div>
-                <div class="dropdown-item complete">
+                <div class="dropdown-item complete complete-btn" data-index= "${index}">
                   <i class="fa-solid fa-check fa-icon"></i>
                   ${task.isCompleted ? "Mark as Active" : "Mark as Completed"}
                 </div>
-                <div class="dropdown-item delete">
+                <div class="dropdown-item delete delete-btn" data-index= "${index}">
                   <i class="fa-solid fa-trash fa-icon"></i>
                   Delete
                 </div>
@@ -69,6 +176,7 @@ function renderTask(tasks) {
     )
     .join("");
 
-  const todoList = $("#todoList");
   todoList.innerHTML = html;
 }
+
+renderTask();
